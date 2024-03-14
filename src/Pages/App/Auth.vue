0df4f6fbfa22ab56029458c2aec77da3b3f0cb19
lnxdxtf <gabrielramosmichaliszen@gmail.com>
2024-03-14 09:27:43 -0300

    auth form on app

--- src/Pages/App/Auth.vue
+++ src/Pages/App/Auth.vue
@@ -1,8 +1,33 @@
 <template>
     <div>
         <div class="w-full h-full flex items-center justify-center">
-            <div class="w-2/4 h-3/4 flex items-center justify-center">
+            <div class="w-2/5 h-3/4 flex items-center justify-center">
                 <div class="w-full h-full p-4 rounded-box bg-base-300 shadow-md">
+
+
+                    <div class="w-full h-full flex flex-col items-center justify-center">
+
+                        <template v-if="!signup">
+                            <div class="w-2/4 flex flex-col gap-4">
+                                <input type="text" class="input input-bordered w-full" placeholder="email">
+                                <input type="password" class="input input-bordered w-full" placeholder="password">
+                                <div class="btn btn-link" @click="signup = !signup">Signup</div>
+                                <div class="btn btn-outline btn-accent">Login</div>
+                            </div>
+                        </template>
+
+                        <template v-else>
+                            <div class="w-2/4 flex flex-col gap-4">
+                                <input type="text" class="input input-bordered w-full" placeholder="email">
+                                <input type="text" class="input input-bordered w-full" placeholder="username">
+                                <input type="password" class="input input-bordered w-full" placeholder="password">
+                                <div class="btn btn-link" @click="signup = !signup">Login</div>
+                                <div class="btn btn-outline btn-accent">Signup</div>
+                            </div>
+                        </template>
+
+                    </div>
+
 
                 </div>
             </div>
@@ -12,6 +37,13 @@
 <script lang="ts">
 import { Component, Vue, toNative } from 'vue-facing-decorator'
 @Component({})
-class Auth extends Vue { }
+class Auth extends Vue {
+
+    public login: string | undefined
+    public password: string | undefined
+
+    public signup: boolean = false
+
+}
 export default toNative(Auth)
 </script>
